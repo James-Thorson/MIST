16b7a89f7c205aca9dd8468d6d2385b9ba734813
Jim Thorson <JamesT.esq@gmail.com>
2017-11-16 06:36:29 -0800

    Small dependency fix to `plot_interactions`

--- R/plot_interactions.R
+++ R/plot_interactions.R
@@ -19,10 +19,10 @@ plot_interactions = function( Report, TmbData, category_names=1:TmbData$n_p, plo
       if( isComplexTF==TRUE ){
         plot( x=1:TmbData$n_p-0.05, y=Re(Eigen$vectors[,p])*sign(mean(Re(Eigen$vectors[,p]))), type="h", lwd=3, ylim=c(-1,1.2), xaxt="n", xlab="", ylab="", xlim=c(1,TmbData$n_p)+c(-0.5,0.5) )
         lines( x=1:TmbData$n_p+0.05, y=Im(Eigen$vectors[,p])*sign(mean(Re(Eigen$vectors[,p]))), type="h", lwd=3, col="red")
-        legend( "top", bty="n", legend=paste0(ThorsonUtilities::FormatC(Re(Eigen$values[p]),3)," ",ifelse(sign(Im(Eigen$values[p]))<0,"-","+")," ",FormatC(abs(Im(Eigen$values[p])),3),"i"), cex=1.2 )
+        legend( "top", bty="n", legend=paste0(ThorsonUtilities::FormatC(Re(Eigen$values[p]),3)," ",ifelse(sign(Im(Eigen$values[p]))<0,"-","+")," ",ThorsonUtilities::FormatC(abs(Im(Eigen$values[p])),3),"i"), cex=1.2 )
       }else{
         plot( x=1:TmbData$n_p, y=Eigen$vectors[,p]*sign(mean(Eigen$vectors[,p])), type="h", lwd=3, ylim=c(-1,1.2), xaxt="n", xlab="", ylab="", xlim=c(1,TmbData$n_p)+c(-0.5,0.5) )
-        legend( "top", bty="n", legend=FormatC(Eigen$values[p],3), cex=1.2 )
+        legend( "top", bty="n", legend=ThorsonUtilities::FormatC(Eigen$values[p],3), cex=1.2 )
       }
       abline( h=0, lty="dotted")
       if( p > (TmbData$n_p-Ncol) ){
