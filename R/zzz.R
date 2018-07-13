a6d4912cd3cc6b7e9525aba9122e60b70fe3c09c
Jim Thorson <JamesT.esq@gmail.com>
2018-07-13 12:40:23 -0700

    remove SpatialDeltaGLMM as dependency

--- R/zzz.R
+++ R/zzz.R
@@ -10,16 +10,16 @@
     packageStartupMessage("Installing INLA...")
     utils::install.packages("INLA", repos="https://www.math.ntnu.no/inla/R/stable")
   }
-  if( !"SpatialDeltaGLMM"%in%utils::installed.packages()[,1] ){
-    packageStartupMessage("Installing package: SpatialDeltaGLMM...")
-    devtools::install_github("nwfsc-assess/geostatistical_delta-GLMM")
-  }
+  #if( !"SpatialDeltaGLMM"%in%utils::installed.packages()[,1] ){
+  #  packageStartupMessage("Installing package: SpatialDeltaGLMM...")
+  #  devtools::install_github("nwfsc-assess/geostatistical_delta-GLMM")
+  #}
   #if( !"TMB" %in% utils::installed.packages()[,1] ){
   #  packageStartupMessage("Installing TMB...")
   #  devtools::install_github("kaskr/adcomp/TMB")
   #}
-  if( !"TMBhelper" %in% utils::installed.packages()[,1] ){
-    packageStartupMessage("Installing package: TMBhelper...")
-    devtools::install_github("kaskr/TMB_contrib_R/TMBhelper")
-  }
+  #if( !"TMBhelper" %in% utils::installed.packages()[,1] ){
+  #  packageStartupMessage("Installing package: TMBhelper...")
+  #  devtools::install_github("kaskr/TMB_contrib_R/TMBhelper")
+  #}
 }
